dc7168adb766bded77608dbc7e316201d467cea3
Eren Kaya <115587114+erenkya@users.noreply.github.com>
2025-07-23 18:38:17 +0300

    Initial Commit

---
+++ src/main/java/com/eren/CloudTrip/CloudTripApplication.java
@@ -0,0 +1,13 @@
+package com.eren.CloudTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudTripApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eren/CloudTrip/CloudTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eren.CloudTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
